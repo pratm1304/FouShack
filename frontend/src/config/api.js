ae43e6df6e2a5d1e04808302ae4a6aba1a8dcfbb
pratm1304 <prathammaske1304@gmail.com>
2025-11-25 12:03:40 +0530

    checking

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,4 +1,4 @@
 // Check if we're in development or production
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001';
+const API_URL = "https://foushack.onrender.com" || 'http://localhost:5001';
 
 export default API_URL;
